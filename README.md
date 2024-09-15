3e406cb13ce401c26c2d795a24947d96a7d990a6
Sergey Petrunichev <bubliksan@gmail.com>
2024-09-16 00:04:59 +0300

    16092024

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Проектная работа Mesto
 
+Использовался макет из курса фулстек-разработки, ссылка: https://www.figma.com/design/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0-1&node-type=canvas&t=fGrPIYwxXjBXCU8d-0
+
+Репозиторий https://github.com/bubliksan/mesto-project-ff.git
+
+Браузер Mozilla Firefox, Pixel Perfect плагин.
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,37 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+
+// функция добавления карточки
+
+function addPlace(placeData, deleteFunc) {
+  const placesList = document.querySelector('.places__list');
+  const placeTemplate = document.querySelector('#card-template').content; // Берём содержимое шаблона
+  const placeElement = placeTemplate.querySelector('.places__item').cloneNode(true); //клонируем
+  // Наполняем
+  placeElement.querySelector('.card__image').src = placeData[0];
+  placeElement.querySelector('.card__image').alt = 'Картинка';
+  placeElement.querySelector('.card__title').textContent = placeData[1];
+  placeElement.querySelector('.card__delete-button').addEventListener('click', deleteFunc) //Обработчик из функции удаления
+  placesList.append(placeElement);
+}
+
+// функция удаления карточки
+
+function deletePlace(evt) {
+  const deleteButton = document.querySelector('.card__delete-button');
+  const place = deleteButton.parentElement;
+  place.remove();
+}
+
+//Запускаем всё
+
+initialCards.forEach(function(item) {
+  let placeMassive = [];
+  placeMassive[0] = item.link;
+  placeMassive[1] = item.name;
+  addPlace(placeMassive, deletePlace);
+})
